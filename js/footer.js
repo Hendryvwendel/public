2b60070a1ee13bcf53333d7a33265bee26ea6a0c
= <=>
2023-02-20 19:38:04 +0100

    Fixed Links on top and fixed footer

--- js/footer.js
+++ js/footer.js
@@ -33,14 +33,14 @@ const createFooter = () => {
             </ul>
         </div>
     </div>
-    <p class="footer-title">about company</p>
+    <p class="footer-title">Over Ons</p>
     <p class="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempore ad suscipit, eos eius quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo deserunt assumenda alias ducimus. Ullam odit maxime id voluptates rerum tenetur corporis laboriosam! Cum error ipsum laborum tempore in rerum necessitatibus nostrum nobis modi! Debitis adipisci illum nemo aperiam sed, et accusamus ut officiis. Laborum illo exercitationem quo culpa reprehenderit excepturi distinctio tempore cupiditate praesentium nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum. Asperiores, natus! Est deserunt incidunt quasi placeat omnis, itaque harum?</p>
     <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
     <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
     <div class="footer-social-container">
         <div>
             <a href="#" class="social-link">terms & services</a>
-            <a href="#" class="social-link">privacy page</a>
+            <a href="#" class="social-link">privacy pagina</a>
         </div>
         <div>
             <a href="#" class="social-link">instagram</a>
